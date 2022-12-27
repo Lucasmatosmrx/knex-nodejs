f682668307b66f37e9f2966dc6e1c22842c4526b
Lucas - 748 <lucas.coelho@cairunet.ad.br>
2022-12-27 12:49:42 -0400

    finalizando rota de update e delete

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -1,8 +1,11 @@
-const express = require('express');
+const express = require("express");
 const routes = express.Router();
 
-const UserController = require('../controllers/UserController')
+const UserController = require("../controllers/UserController");
 
-routes.get('/users',UserController.index)
+routes.get("/users", UserController.index);
+routes.post("/users", UserController.create);
+routes.put("/users/:id", UserController.update);
+routes.delete("/users/:id", UserController.delete);
 
-module.exports = routes
+module.exports = routes;
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,13 @@ const routes = require('./routes/users');
 const app = express();
 const port = 3333
 
+app.use(express.json())
 app.use(routes)
+
+// catch all
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    res.json({error: error.message})
+})
 
 app.listen(port, () => console.log(`Server is runing port ${port}`))

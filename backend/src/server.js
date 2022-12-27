8d0cb8e19cb6cfce9a3cea0eb55654b75ce5df3f
Lucas - 748 <lucas.coelho@cairunet.ad.br>
2022-12-27 12:56:06 -0400

    add not found error 404

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,22 @@
-const express = require('express');
-const routes = require('./routes/users');
+const express = require("express");
+const routes = require("./routes/users");
 const app = express();
-const port = 3333
+const port = 3333;
 
-app.use(express.json())
-app.use(routes)
+app.use(express.json());
+app.use(routes);
+
+// notFound
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
 
 // catch all
 app.use((error, req, res, next) => {
-    res.status(error.status || 500)
-    res.json({error: error.message})
-})
+  res.status(error.status || 500);
+  res.json({ error: error.message });
+});
 
-app.listen(port, () => console.log(`Server is runing port ${port}`))
+app.listen(port, () => console.log(`Server is runing port ${port}`));

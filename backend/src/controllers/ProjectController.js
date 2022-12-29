43f5f3072a34a6ffff9d7b91965fd342afef1051
Lucas - 748 <lucas.coelho@cairunet.ad.br>
2022-12-29 09:57:47 -0400

    add join table users is projects

--- backend/src/controllers/ProjectController.js
+++ backend/src/controllers/ProjectController.js
@@ -3,10 +3,27 @@ const knex = require("../database");
 module.exports = {
   async index(req, res, next) {
     try {
-      const results = await knex("projects");
+      const { user_id } = req.query;
+
+      const query = knex("projects");
+
+      if (user_id) {
+        query
+          .where({ user_id })
+          .join("users", "users.id", "=", "projects.user_id")
+          .select("projects.*", "users.username");
+      }
+
+      const results = await query;
+
       return res.json(results);
     } catch (error) {
       next(error);
     }
   },
+  async create(req, res, next){
+    try{
+
+    }catch(error)
+  }
 };
